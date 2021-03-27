7e8252e7c3f07a8cef5b45a04c80d1145c2884aa
Ekaterina Kozlova <ekaterina.kozlova@sbb.ch>
2021-03-27 23:15:52 +0300

    Stage 1

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -4,4 +4,8 @@ first_month = 'Month 1: repaid 250'
 second_month = 'Month 2: repaid 250'
 third_month = 'Month 3: repaid 500'
 
-# write your code here
+print(loan_principal)
+print(first_month)
+print(second_month)
+print(third_month)
+print(final_output)
